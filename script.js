72c4f61edd967ded17094437a5c95e701199d84f
Manojparm <123813997+Manojparm@users.noreply.github.com>
2023-07-12 14:26:00 +0530

    responsive navbar

---
+++ script.js
@@ -0,0 +1,13 @@
+let sidemenu=document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemeu(){
+    sidemenu.style.right="-200px"
+}
+
+let closee=document.getElementById("sidemenu");
+function hello1(){
+    closee.style.right="-200px"
+}
